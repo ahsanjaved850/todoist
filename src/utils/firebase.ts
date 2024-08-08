1ce05b31db8e73421f81e702f741e48d28ca6e63
Ahsan Javed <jahsan850@gmail.com>
2024-08-08 15:49:20 +0500

    change

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -7,15 +7,6 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBRf19uffdpypq3TzUvexMvORKAh3Q3V_8",
-  authDomain: "todoist-ae3a3.firebaseapp.com",
-  projectId: "todoist-ae3a3",
-  storageBucket: "todoist-ae3a3.appspot.com",
-  messagingSenderId: "488604132119",
-  appId: "1:488604132119:web:bbd50484f49ec49d96cd52",
-  measurementId: "G-T1DQXYM7HV",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
