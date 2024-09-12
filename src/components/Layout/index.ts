954c69f922b2d686542927fd754e8ffe9cda8263
Ahsan Javed <jahsan850@gmail.com>
2024-09-12 22:15:31 +0500

    1 done

--- src/components/Layout/index.ts
+++ src/components/Layout/index.ts
@@ -1,12 +1,2 @@
 export { Layout } from "./Layout";
 export { FeaturesContainer } from "./FeaturesContainer";
-export {
-  LayoutContainer,
-  FeaturesWindow,
-  Name,
-  AddTask,
-  Today,
-  Project,
-  Overlay,
-  ProjectNameList,
-} from "./Layout.style";
